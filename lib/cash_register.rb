54479d76c1dac6a18e98c6e47051791b91ecd07d
Chandon Porter <cneilporter94@gmail.com>
2019-08-10 19:24:09 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,20 @@
+class CashRegister
+  
+  attr_accessor :total, :discount
+  
+def initialize(discount = 0)
+  @discount = discount
+  @total = 0
+  
+end
+
+def total
+  @total
+  return total
+end 
+
+def add_item
+  @
+
+end
 
